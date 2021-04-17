2acefb10e97c79725745217e5e822c03310f1453
Lord Ashes <webtable@gmail.com>
2021-04-17 14:34:56 -0400

    Commit the database folder of VirtualTable3D
    Commit the database folder of VirtualTable3D

---
+++ database/migrations/2021_04_13_182525_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+			$table->string('module');
+			$table->string('source');
+			$table->string('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
