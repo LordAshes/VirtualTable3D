4860f51da2f13bbe9df3187373f4b90fe25d9db6
Lord Ashes <webtable@gmail.com>
2021-04-26 22:34:12 -0400

    Updated for multi line rolls
    Updated to support multi line rolls

--- resources/views/components/CharacterSheets.blade.php
+++ resources/views/components/CharacterSheets.blade.php
@@ -2,18 +2,19 @@
 			 * Character Sheet functionality inserted from views/componets/CharacterSheets.blade.php *
 			 ****************************************************************************************/
 			 
+			 
+			var rollType = document.createElement("div");
+			var content = "<input type=radio name='rollType' id=Adv value='Adv'><label for='Adv'>Adv</label>"
+						+ "<input type=radio name='rollType' id=Nor value='Nor' checked=true><label for='Nor'>Nor</label>"
+						+ "<input type=radio name='rollType' id=Dis value='Dis'><label for='Dis'>Dis</label>";
+			rollType.innerHTML = content;
+			rollType.style ='Position: Absolute; Left: 1755px; Top: 10px; color: yellow;';
+			document.body.appendChild(rollType);
+
 			updateCall(function(content)
 			{
 				eval(content);
-				
-				var rollType = document.createElement("div");
-				var content = "<input type=radio name='rollType' id=Adv value='Adv'><label for='Adv'>Adv</label>"
-							+ "<input type=radio name='rollType' id=Nor value='Nor' checked=true><label for='Nor'>Nor</label>"
-							+ "<input type=radio name='rollType' id=Dis value='Dis'><label for='Dis'>Dis</label>";
-				rollType.innerHTML = content;
-				rollType.style ='Position: Absolute; Left: 1755px; Top: 10px; color: yellow;';
-				document.body.appendChild(rollType);
-				
+								
 				var options = document.createElement("select");
 				options.id = "options";
 				options.style = "Position: Absolute; Left: 1760px; Top: 35px; color: yellow; background-color: blue;"; 
@@ -22,9 +23,8 @@
 					if(document.getElementById("options").value!="Select Roll")
 					{
 						var skill = document.getElementById("options").value;
-						document.getElementById("Roll").value = rolls[skill];
-						document.getElementById("options").value = "";
-						onRollExecute(null,skill);
+						document.getElementById("options").value="";
+						onRollExecute(rolls[skill],skill);
 					}
 				});
 										
