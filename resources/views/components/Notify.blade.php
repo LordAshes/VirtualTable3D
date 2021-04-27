b74074f2ce54e170f788097be2d9a6074d5182dd
Lord Ashes <webtable@gmail.com>
2021-04-26 22:36:06 -0400

    Updated to remove player prefix
    Updated to remove player prefix which is not added in the dice component

--- resources/views/components/Notify.blade.php
+++ resources/views/components/Notify.blade.php
@@ -5,7 +5,7 @@
 			var notifyPulse = null;
 
 			// Hold current notification entries
-			var numChatEntries = 30;
+			var numChatEntries = 55;
 			var numChatShow = numChatEntries;
 			var chat=[];
 			
@@ -13,7 +13,7 @@
 			var tooltip = document.createElement("span");
 			tooltip.id = "tooltip";
 			tooltip.name = "tooltip";
-			tooltip.style = "position: absolute; left: 0px; top: 0px; color: #ADD8E6; background-color: black; z-index: 100; visibility: hidden;";
+			tooltip.style = "position: absolute; left: 0px; top: 0px; color: #ADD8E6; background-color: black;  font-family: 'Lucida Console', 'Courier New', monospace; z-index: 100; visibility: hidden;";
 			tooltip.addEventListener('mouseover',function(e){e.preventDefault(); e.stopPropagation();});
 			tooltip.addEventListener('mousedown',function(e){e.preventDefault(); e.stopPropagation(); adjustChat();});
 			document.body.appendChild(tooltip);
@@ -25,35 +25,26 @@
 				var el = document.createElement("div");
 				el.id = "Chat"+i;
 				el.name = "Chat"+i;
-				el.style = "position: absolute; left: 10px; top: "+(45+i*30)+"px; height: 25px; padding-top: 5px; width: 300px; color: #ADD8E6; background-color: rgba(0, 0, 0, 0.5);";			
+				el.style = "position: absolute; left: 10px; top: "+(45+i*16)+"px; height: 16px; padding-top: 0px; width: 310px; color: #ADD8E6; background-color: rgba(0, 0, 0, 0.5); font-family: 'Lucida Console', 'Courier New', monospace;";			
 				el.addEventListener('mouseenter',function(e){e.preventDefault(); e.stopPropagation(); showTooltip(this);});
 				document.body.appendChild(el);
 				chat.push({short: "", full: ""});
 			}
 				
-			function notify(txt, interval)
+			function notify(short, full)
 			{
-				updateSend("notify_basic(\""+encodeURIComponent(txt)+"\","+interval+");\r\n",csrf);
+				updateSend("notify_basic(\""+encodeURIComponent(short)+"\",\""+encodeURIComponent(full)+"\");\r\n",csrf);
 			}
-			
-			function abc()
+						
+			function notify_basic(short,full)
 			{
-				console.log(chat);
-			}
-			
-			function notify_basic(txt, interval)
-			{
-				var short = txt.replace(/<[^>]*>/g, '');
-				if(short.indexOf(":")>1)
-				{
-					short = short.substring(0,short.indexOf(":")+1)+" "+short.substring(short.lastIndexOf("=")+1);
-				}
-				chat.push({short: short, full: txt});
+				chat.push({short: short, full: full});
 				chat.shift();
 				for(var i=0; i<numChatShow; i++)
 				{
 					showNotice(document.getElementById("Chat"+i));
 				}
+				console.log(chat);
 			}
 						
 			function showNotice(dst)
@@ -65,11 +56,14 @@
 			function showTooltip(dst)
 			{
 				var id = parseInt(numChatEntries)-parseInt(dst.id.substring(4))-1;
-				var el = document.getElementById("tooltip");
-				el.style.left = (parseInt(dst.style.left)+10)+"px"
-				el.style.top = (parseInt(dst.style.top)+10)+"px"
-				el.style.visibility = "visible";
-				el.innerHTML = "&nbsp;"+chat[id].full.substring(chat[id].full.indexOf(":")+1).trim();
+				if(chat[id].full.indexOf("&boxh")<0)
+				{
+					var el = document.getElementById("tooltip");
+					el.style.left = (parseInt(dst.style.left)+20)+"px"
+					el.style.top = (parseInt(dst.style.top)+0)+"px"
+					el.style.visibility = "visible";
+					el.innerHTML = "&nbsp;"+chat[id].full.substring(chat[id].full.indexOf(":")+1).trim();
+				}
 			}
 			
 			function clearTooltip(dst)
@@ -85,19 +79,9 @@
 				{
 					document.getElementById("Chat"+i).style.visibility = "hidden";
 				}
-				if(numChatShow==numChatEntries){numChatShow=1;}else{numChatShow=numChatEntries;}
+				if(numChatShow==numChatEntries){numChatShow=3;}else{numChatShow=numChatEntries;}
 				for(var i=0; i<numChatShow; i++)
 				{
 					document.getElementById("Chat"+i).style.visibility = "visible";
 				}				
 			}
-
-			function guid()
-			{
-				return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
-				{
-					var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-					return v.toString(16);
-				});
-			}
-
