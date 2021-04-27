57da08785fb2df638270302ab0d1c98f08c3b61f
Lord Ashes <webtable@gmail.com>
2021-04-26 22:35:06 -0400

    Updated to support multi line rolls
    Updated to support box formatted multi line rolls

--- resources/views/components/Dice.blade.php
+++ resources/views/components/Dice.blade.php
@@ -11,23 +11,58 @@
 			
 			function onRollChange(e)
 			{
-				onRollExecute(e, "Custom")
-			}
-			
-			function onRollExecute(e,skill)
-			{
 				var specs = document.getElementById("Roll").value;
 				document.getElementById("Roll").value = "";
 				
+				onRollExecute(specs, specs)
+			}
+			
+			function onRollExecute(specs,skill)
+			{
 				if(document.getElementById("Adv").checked==true){while(specs.indexOf("Nor(")>=0){specs = specs.replace("Nor(","Adv(");}}
 				if(document.getElementById("Dis").checked==true){while(specs.indexOf("Nor(")>=0){specs = specs.replace("Nor(","Dis(");}}
+								
+				var max = 28;
+				var rep = (max-(("("+player+")").length))/2;
 				
-				notify("["+player+"] "+skill+": " +roll(specs, character),10000);
+				var header = "";
+				var content = "";
+				var footer = "";
+				var pattern = "";
+				for(var i=0; i<max; i++){header=header+"-"; pattern=pattern+" ";}
+				footer = header;
+				header = "<span style='color: #4D7896'>&boxdr;"+header.substring(0,rep)+"("+player+")"+header.substring(rep + ("|"+player+"|").length)+"&boxdl;</span>";
+				footer = "<span style='color: #4D7896'>&boxur;"+footer+"&boxul;</span>";
+				while(footer.indexOf("-")>-1){footer = footer.replace("-","&boxh;"); header = header.replace("-","&boxh;");}
+				
+				notify(footer,footer);
+				var specParts = specs.split("|");
+				for(var p=specParts.length-1; p>=0; p--)
+				{
+					content = roll(specParts[p], character);
+					var short = content.replace(/<[^>]*>/g, '').replace("&nbsp;"," ").replace("&nbsp"," ");
+					short = short.substring(short.lastIndexOf("=")+1).trim();
+					if(short.indexOf(":")>-1)
+					{
+						short = short.replace(":"," =");
+					}
+					else
+					{
+						short = skill + " = " + short;
+					}
+					while(short.length<max){short = " "+short+" ";}
+					short = short.substring(0,max);
+					while(short.indexOf(" ")>-1){short = short.replace(" ","&nbsp");}
+					short = "<span style='color: #4D7896'>&boxv;</span>"+short+"<span style='color: #4D7896'>&boxv;</span>";
+					notify(short,content);
+				}
+				notify(header,header);				
 			}
 
 			function roll(specs, info)
 			{				
 				var calc = specs;
+				specs = resolveText(specs);
 				specs = resolveAdvAndDis(specs);
 				if(info!=undefined)
 				{
@@ -51,6 +86,36 @@
 					specs = specs.replace("Math.min","Dis");
 				}
 				return specs.toPascalCase() + " = " + "<SPAN class='Normal'>"+eval(calc)+"</SPAN>&nbsp";
+			}
+			
+			function resolveText(specs)
+			{
+				var result = "";
+				var escaped = false;
+				for(var i=0; i<specs.length; i++)
+				{
+					if(specs.substring(i,i+1)!="D" || escaped)
+					{
+						result = result + specs.substring(i,i+1);
+						if(specs.substring(i,i+1)=="'")
+						{
+							escaped = !escaped;
+						}
+						else if(specs.substring(i,i+1)=="{")
+						{
+							escaped = true;
+						}
+						else if(specs.substring(i,i+1)=="}")
+						{
+							escaped = false;
+						}								
+					}
+					else
+					{
+						result = result + "~";
+					}
+				}
+				return result;
 			}
 			
 			function resolveAdvAndDis(specs)
@@ -99,9 +164,9 @@
 			
 			function resolveDice(specs)
 			{
-				while(specs.indexOf("D")>-1)
+				while(specs.indexOf("~")>-1)
 				{
-					var pos = specs.indexOf("D");
+					var pos = specs.indexOf("~");
 					var prefix = pos-1;
 					while(prefix>-1 && "0123456789".indexOf(specs.substring(prefix,prefix+1))>-1) { prefix--; }
 					prefix++;
@@ -111,7 +176,7 @@
 					var suffix = pos+1;
 					while(suffix<specs.length && "0123456789".indexOf(specs.substring(suffix,suffix+1))>-1) { suffix++; }
 					var dice_sides = specs.substring(pos+1,suffix);
-					var desc = "<!-- "+num_dice+"_"+dice_sides+" -->[";
+					var desc = "<!-- "+num_dice+"D"+dice_sides+" -->[";
 					for(var dice=0; dice<Math.max(num_dice_int,1); dice++)
 					{
 						var roll = parseInt(Math.random() * parseInt(dice_sides)) + 1;
@@ -129,9 +194,9 @@
 						}
 						if(dice<(num_dice-1)){desc=desc+"+";}else{desc=desc+"]";}
 					}
-					specs = specs.replace(num_dice+"D"+dice_sides,desc);
+					specs = specs.replace(num_dice+"~"+dice_sides,desc);
 				}
-				while(specs.indexOf("_")>-1){specs = specs.replace("_","D");}
+				// while(specs.indexOf("_")>-1){specs = specs.replace("_","D");}
 				return specs;
 			}
 			
